b1682ae7c637268a7ec559f2b72df9e6a53eb5a4
Vladislav <vladislavturok@MacBook-Air-Vladislav.local>
2015-05-04 19:30:53 +0300

    add plagin

--- js/script.js
+++ js/script.js
@@ -1,58 +1,66 @@
-$(document).ready(function(){
-	rangeSlider()
-	$(window).resize(function(){
-		rangeSlider();
-	})
-	function rangeSlider() {
-		$('.slider-wrap .slider-inner').each(function(){
-			var thisSlideW = $(this).find('.slide').outerWidth(true);
-			var thisSlideSize = $(this).find('.slide').size();
+(function($){
+	$.fn.rangePlugin = function(options){
+	 	var opts = $.extend({
+	 		sliderName: '.slider',
+	 		sliderInner: '.slider-inner',
+	 		slideName: '.slide',
+	 		rangeWrap: '.range-slider',
+	 		rangeTrack: '.range-track',
+	 		rangeDrag: '.range-drag'
+		}, options);
+		$(opts.sliderInner).each(function(){
+			var thisSlideW = $(this).find(opts.slideName).outerWidth(true);
+			var thisSlideSize = $(this).find(opts.slideName).size();
 			$(this).width(thisSlideW*thisSlideSize);
+			console.log(thisSlideW)
 		});
-		$('.range-slider').click(function(e){
+		$(opts.rangeWrap).click(function(e){
 			var rangeW = $(this).width();
-			var slideW = $(this).parents('.slider-wrap').find('.slider').width();
-			var offsetRengeX = $(this).offset().left
-			var sliderW = $(this).parents('.slider-wrap').find('.slider-inner').width()-slideW;
-			var cursorPos = e.pageX - offsetRengeX;
+			var sliderW = $(this).prev().width();
+			var offsetRangeX = $(this).offset().left
+			var slideInnerW = $(this).prev().find(opts.sliderInner).width()-sliderW;
+			var cursorPos = e.pageX - offsetRangeX;
 			var cursorProc = cursorPos*100;
 			var posProc = cursorProc/rangeW;
-			var sliderProc = sliderW/100;
+			var sliderProc = slideInnerW/100;
 			if (cursorPos > rangeW || cursorPos < 0) {
 				return
 			};
-			$(this).find('.range-drag').animate({'left' : cursorPos}, 300);
-			$(this).find('.range-track').animate({'width' : cursorPos}, 300);
-			$(this).parents('.slider-wrap').find('.slider-inner').animate({
+			$(this).find(opts.rangeDrag).animate({'left' : cursorPos}, 300);
+			$(this).find(opts.rangeTrack).animate({'width' : cursorPos}, 300);
+			$(this).prev().find(opts.sliderInner).animate({
 				'left' : -sliderProc*posProc
 			}, 300);
 		});
-
-		$('.range-drag').mousedown(function(){
-			$(this).parents('.slider-wrap').addClass('mouse-down');
-			var rangeW = $(this).parent().width();
-			var slideW = $(this).parents('.slider-wrap').find('.slider').width();
-			console.log(slideW);
-			var offsetRengeX = $(this).parent().offset().left
-			var sliderW = $(this).parents('.slider-wrap').find('.slider-inner').width()-slideW;
-			$(document).mousemove(function(e){
-				var cursorPos = e.pageX - offsetRengeX;
+		$(this).find(opts.rangeWrap).mousedown(function(){
+			$(this).prev().addClass('mouse-down');
+			var rangeW = $(this).width();
+			var sliderW = $(this).prev().width();
+			var offsetRangeX = $(this).offset().left
+			var slideInnerW = $(this).prev().find(opts.sliderInner).width()-sliderW;
+			var sliderProc = slideInnerW/100;
+			$('.mouse-down').parent().mousemove(function(e){
+				var cursorPos = e.pageX - offsetRangeX;
 				var cursorProc = cursorPos*100;
 				var posProc = cursorProc/rangeW;
-				var sliderProc = sliderW/100;
 				if ( cursorPos > rangeW || cursorPos < 0) {
 					return
 				};
-				$('.mouse-down').find('.range-drag').css({'left' : cursorPos}, 300);
-				$('.mouse-down').find('.range-track').css({'width' : cursorPos}, 300);
-				$('.mouse-down').find('.slider-inner').css({
+				$('.mouse-down').next().find(opts.rangeDrag).css({'left' : cursorPos}, 300);
+				$('.mouse-down').next().find(opts.rangeTrack).css({'width' : cursorPos}, 300);
+				$('.mouse-down').find(opts.sliderInner).css({
 					'left' : -sliderProc*posProc
 				}, 300);
 			});
 			return false
 		});
 		$(document).mouseup(function(){
-			$('.slider-wrap').removeClass('mouse-down');
+			$(opts.sliderName).removeClass('mouse-down');
 		});
-	}
-});
+	};
+})(jQuery);
+
+
+
+
+
